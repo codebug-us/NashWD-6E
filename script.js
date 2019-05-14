c96b55e03b8e4a3261783ea5867ccbf75d089d97
aemiller2013 <alexandra.e.miller@vanderbilt.edu>
2019-05-14 10:28:34 -0500

    completed tasks

--- script.js
+++ script.js
@@ -38,47 +38,98 @@ $(document).ready(function(){
 
 	// Your code goes below: 
 	// Make your companies array
-
+	var companies=[tesla, lulu, apple, amazon];
 
 
 	// Print each company's ticker symbol
-
+	for(var i=0;i<companies.length;i++){
+		console.log(companies[i].ticker);
+	}
 
 
 
 
 	// Print each company's share price
-
+	for(i=0;i<companies.length;i++){
+		console.log(companies[i].sharePrice);
+	}
 
 
 
 
 	// Print each company's CEO (using a forEach loop)
-
+	companies.forEach(function(company){
+		console.log(company.ceo);
+	});
 
 
 
 	
 	// Calculate the average share price (forEach)
-
+	var avgSharePrice=0;
+	companies.forEach(function(company){
+		avgSharePrice=avgSharePrice+company.sharePrice;
+	});
+	avgSharePrice=avgSharePrice/4;
+	console.log(avgSharePrice);
 
 
 
 
 	// Calculate the profit
+	companies.forEach(function(company){
+		company.profit=(company.revenue-company.costs);
+	});
+	companies.forEach(function(company){
+		console.log(company.profit);
+	});
 
-
-
+	console.log("above is profit***********below is rev");
 
 
 	// The stock market is crashing! Lower revenues by 50%. 
+	companies.forEach(function(company){
+		company.revenue=company.revenue*.5;
+	});
+	companies.forEach(function(company){
+		console.log(company.revenue);
+	});
 
+	console.log("above rev**********below is incorrect profit");
 
+	companies.forEach(function(company){
+		//print each profit
+		console.log(company.profit);
+	});
 
-
-
+	console.log("above is wrong profit**********below is good profit");
 
 	// Make the profit calculation dynamic
+	var calculateProfit=function(){
+		return (this.revenue-this.costs);
+	}
+	companies.forEach(function(company){
+		company.profit=calculateProfit;
+		console.log(company.profit());
+	});
+
+	// console.log("REV & COST************");
+
+	// companies.forEach(function(company){
+	// 	console.log(company.revenue+" & "+company.costs);
+	// });
+
+	console.log("~~~~~~~~~~~~~~");
+
+	//test the profits with changed rev and costs
+	companies.forEach(function(company){
+		company.revenue=100;
+		company.costs=70;
+		console.log(company.profit());
+	});
+
+
+
 
 
 
